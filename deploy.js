f13765afaaffe1c1206f6007d743a396f6c84b83
Alycia Crane <alyciacrane@Alycias-MacBook-Pro.local>
2020-05-16 18:06:55 -0500

    updated IP

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '184.73.59.135',
+      host: '54.234.166.11',
       username: 'ubuntu',
       privateKey: 'HandsOnL01key.pem'
     })
